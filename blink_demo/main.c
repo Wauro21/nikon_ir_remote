3dc18599974650ed3726bde197e96701fed3c4d5
Mauricio <maurio.aravena@sansano.usm.cl>
2023-08-17 00:50:25 -0400

    Added full example on the pipeline for the IC

---
+++ blink_demo/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main (void){
+    /* set pin 5 of PORTB for output*/
+    DDRB |= _BV(DDB3);
+    
+    
+    while(1) {
+    /* set pin 5 high to turn led on */
+    PORTB |= _BV(PORTB3);
+    _delay_ms(BLINK_DELAY_MS);
+
+    /* set pin 5 low to turn led off */
+    PORTB &= ~_BV(PORTB3);
+    _delay_ms(BLINK_DELAY_MS);
+ }
+}
---
+++ blink_demo/main.h
@@ -0,0 +1,10 @@
+#ifndef DEMO_H_
+#define DEMO_H_
+
+#define __AVR_ATtiny85__
+#define F_CPU 8000000 // 1 Mhz for testing
+#define BLINK_DELAY_MS 500
+
+
+
+#endif
