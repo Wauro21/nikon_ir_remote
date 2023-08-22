9db7fae694c52f6cc9645f4802a1bdd380833be5
Mauricio <maurio.aravena@sansano.usm.cl>
2023-08-22 18:22:19 -0400

    another demo

--- ir_code/main.c
+++ ir_code/main.c
@@ -1,50 +1,36 @@
 #include "main.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/cpufunc.h>
-
-volatile uint8_t state = 0;
 
 int main(void)
 {
 
-    // Configure output pins
-    DDRB |= _BV(PB0); // Set pin as an output
-    DDRB |= _BV(PB1);
-    DDRB |= _BV(PB2);
-
+    DDRB |= _BV(PB0) | _BV(PB1);
     PORTB &= ~_BV(PB1);
 
-    // Try using the timer0 to generate the 38.4 kHz carrier
-    // [CONFIGURE TIMER]
-    // -> GTCCR - General timer configs
-    GTCCR &= ~_BV(TSM);
-    GTCCR &= ~_BV(PSR0);
+    // Set general register to sync mode for configuration
+    // GTCCR |= _BV(TSM) | _BV(PSR0);
 
-    // -> Timer registers
-    TCCR0A = 0;
+    // Default registers
+    TCCR0A = 0; 
     TCCR0B = 0;
-    TCCR0A |= _BV(WGM01);  // Set CTC mode of operation
-    TCCR0A |= _BV(COM0A0); // Toggle on Compare match -> PB0 (Pin5)
+    TCNT0 = 0;
 
-    TCCR0B |= _BV(CS00); // internal clock with no prescaling
+    TCCR0A |= _BV(COM0A0) | _BV(WGM01); // Set toggle on match and CTC mode
+    TCCR0B |= _BV(CS00); // internal clock no-prescaling
 
-    TCNT0 = 0; // Start the counter at zero
-
-    OCR0A = 12; // For 38,462kHz
+    OCR0A |= 12; // For app 38,8kHz
 
     TIMSK |= _BV(OCIE0A);
 
+    // Enable timer
 
-    while(1){
-        PORTB ^= _BV(PB2);
-    }
+    // GTCCR &= ~_BV(TSM) & ~_BV(PSR0);
+    while(1);
+
 
 }
 
-
-ISR(TIMER0_COMPA_vect)
-{
+ISR(TIMER0_COMPA_vect){
     PORTB ^= _BV(PB1);
-    
 }
