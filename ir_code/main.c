2e7b1b02e772b3ea318ac92bec582cb47a9ef4ad
Mauricio <maurio.aravena@sansano.usm.cl>
2023-08-24 23:27:01 -0400

    funcional mockup

--- ir_code/main.c
+++ ir_code/main.c
@@ -3,7 +3,7 @@
 
 volatile uint16_t isr_counter = 0U;
 volatile uint8_t state = 0U;
-volatile uint8_t round_count = 0;
+volatile uint8_t enable = 0U;
 
 int main(void)
 {
@@ -18,139 +18,99 @@ int main(void)
     TIMSK = 0;
     TCCR0A |= _BV(COM0A0) | _BV(WGM01); // Set toggle on match and CTC mode
     TCCR0B |= _BV(CS00);                // internal clock no-prescaling
-    OCR0A = 103;                        // For app 38,8kHz
+    OCR0A = 104;                        // For app 38,8kHz
     TIMSK |= _BV(OCIE0A);
     // Enable timer
     GTCCR &= ~_BV(TSM) & ~_BV(PSR0);
     // Enable global interrupts
     sei();
-    while (1);
+    while (1){
+        if(enable == 1) {
+            TCCR0A |= _BV(COM0A0);
+            enable = 0;
+        }
+
+        else if(enable == 2) {
+            TCCR0A &= ~_BV(COM0A0);
+            enable = 0;
+        }
+    };
 }
 // Demo interrupt service routine
 ISR(TIMER0_COMPA_vect)
 {
     switch (state)
     {
-    case 0U:
-        if (isr_counter < 151U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A &= ~_BV(COM0A0);
-            isr_counter = 0U;
-            state = 1U;
-        }
-        break;
-
-    case 1U:
-        if (isr_counter < 2127U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A |= _BV(COM0A0);
-            isr_counter = 0U;
-            state = 2U;
-        }
-        break;
-
-    case 2U:
-        if (isr_counter < 29U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A &= ~_BV(COM0A0);
-            isr_counter = 0U;
-            state = 3U;
-        }
-        break;
-
-    case 3U:
-        if (isr_counter < 119U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A |= _BV(COM0A0);
-            isr_counter = 0U;
-            state = 4U;
-        }
-        break;
-
-    case 4U:
-        if (isr_counter < 29U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A &= ~_BV(COM0A0);
-            isr_counter = 0U;
-            state = 5U;
-        }
-        break;
-
-    case 5U:
-        if (isr_counter < 271U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A |= _BV(COM0A0);
-            isr_counter = 0U;
-            state = 6U;
-        }
-        break;
-
-    case 6U:
-        if (isr_counter < 29U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A &= ~_BV(COM0A0);
-            isr_counter = 0U;
-            state = 7U;
-        }
-        break;
-
-    case 7U:
-        if (isr_counter < 4854U)
-            isr_counter += 1U;
-        else
-        {
-            if(round_count < 1U){
-                TCCR0A |= _BV(COM0A0);
-                state = 0U;
-                round_count += 1U;
-            }
+        case 0:
+            if(isr_counter < 151U) isr_counter += 1;
             else {
-                state = 8U;
-                round_count = 0U;
+                enable = 2U;
+                isr_counter = 0;
+                state = 1;
             }
-            isr_counter = 0U;
-            
-        }
-        break;
+            break;
 
-    case 8U:
-        if (isr_counter < 65000U)
-            isr_counter += 1U;
-        else
-        {
-            TCCR0A |= _BV(COM0A0);
-            isr_counter = 0U;
-            state = 0U;
-        }
-        break;
+        case 1:
+            if(isr_counter < 2127U) isr_counter += 1;
+            else {
+                enable = 1U;
+                isr_counter = 0;
+                state = 2;
+            }
+            break;
 
+        case 2:
+            if(isr_counter < 29U) isr_counter += 1;
+            else {
+                enable = 2U;
+                isr_counter = 0;
+                state = 3;
+            }
+            break;
+
+        case 3:
+            if(isr_counter < 119U) isr_counter += 1;
+            else {
+                enable = 1U;
+                isr_counter = 0;
+                state = 4;
+            }
+            break;
+        
+        case 4:
+            if(isr_counter < 29U) isr_counter += 1;
+            else {
+                enable = 2U;
+                isr_counter = 0;
+                state = 5;
+            }
+            break;
+
+        case 5:
+            if(isr_counter < 271U) isr_counter += 1;
+            else {
+                enable = 1U;
+                isr_counter = 0;
+                state = 6;
+            }
+            break;
+
+        case 6:
+            if(isr_counter < 29U) isr_counter += 1;
+            else {
+                enable = 2U;
+                isr_counter = 0;
+                state = 7;
+            }
+            break;
+
+        case 7:
+            if(isr_counter < 4853U) isr_counter += 1;
+            else {
+                enable = 1U;
+                isr_counter = 0;
+                state = 0;
+            }
+            break;
     }
-    // if(isr_counter < 19) isr_counter += 1;
-    // else{
-    //     if(state == 0) {
-    //         TCCR0A &= ~_BV(COM0A0);
-    //         state = 1;
-    //     }
-    //     else
-    //     {
-    //         TCCR0A |= _BV(COM0A0);
-    //         state = 0;
-    //     }
-    //     // TCCR0A ^= _BV(COM0A0);
-    //     isr_counter = 0;
-    // }
 }
