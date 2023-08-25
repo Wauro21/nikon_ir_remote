bdf632657c1a247f1b45bbf179c8ebb1e1a85738
Mauricio <maurio.aravena@sansano.usm.cl>
2023-08-25 00:18:30 -0400

    Cleaning code

--- ir_code/functions.h
+++ ir_code/functions.h
@@ -1,6 +1,14 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+/// @brief Defines the possibles states controlling the Timer0 pinout
+typedef enum {
+    IDLE    = 0U,
+    ON      = 1U,
+    OFF     = 2U,
+} t0_pinout_states;
+
+
 /// @brief Configures timer 0 to generate a 38kHz square wave.
 /// @param  void
 void setupTimer0(void);
